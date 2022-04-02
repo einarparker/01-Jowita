f4d05840f74148fe6e6014d1d329b3365b69030a
einarparker <76267622+einarparker@users.noreply.github.com>
2022-04-02 21:49:13 +0100

    add menu, hamburger animation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function menuOnClick() {
     document.getElementById("menu-box").classList.toggle("change");
-    // document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-small").classList.toggle("change-menu");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
