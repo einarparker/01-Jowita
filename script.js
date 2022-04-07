e8b7bf675b76f78806e6bdb12c8a226141939509
einarparker <76267622+einarparker@users.noreply.github.com>
2022-04-07 20:07:14 +0100

    remove js

--- script.js
+++
@@ -1,5 +0,0 @@
-function menuOnClick() {
-    document.getElementById("menu-box").classList.toggle("change");
-    document.getElementById("menu-small").classList.toggle("change-menu");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
